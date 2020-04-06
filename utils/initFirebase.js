81bb3612081381ed7d8f69063fe1611f93b622a7
Milson Júnior <junior.milson17@gmail.com>
2020-04-05 22:48:52 -0300

    fix: service-account key env var

--- utils/initFirebase.js
+++ utils/initFirebase.js
@@ -1,12 +1,14 @@
 import * as admin from 'firebase-admin'
 
+const atob = require('atob')
+
 function initFirebase() {
   admin.initializeApp({
     credential: admin.credential.cert({
       type: process.env.GOOGLE_SERVICE_ACCOUNT_TYPE,
       project_id: process.env.GOOGLE_SERVICE_ACCOUNT_PROJECT_ID,
       private_key_id: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
-      private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
+      private_key: atob(process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY),
       client_email: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
       client_id: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_ID,
       auth_uri: process.env.GOOGLE_SERVICE_ACCOUNT_AUTH_URI,
